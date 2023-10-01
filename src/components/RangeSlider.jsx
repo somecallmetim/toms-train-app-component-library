4d30be9dd39b07bb757ec57a733a6b64e7a5de0f
Tim Bauer <timbauer@ymail.com>
2023-10-01 13:43:02 -0700

    slider with basic animations complete

--- src/components/RangeSlider.jsx
+++ src/components/RangeSlider.jsx
@@ -1,10 +1,31 @@
+import { useState } from "react";
+
 function RangeSlider() {
+  const [sliderValue, setSliderValue] = useState(0);
+
+  const sliderMax = 100;
+
+  function onInput(e) {
+    setSliderValue((e.target.value / sliderMax)*100);
+  }
+
   return (
     <>
-        <input type="range" name="test" id="test1" />
+      <input
+        type="range"
+        name="test"
+        id="range"
+        min={0}
+        max={sliderMax}
+        style={{
+          background: `linear-gradient(to right, #8b4513 ${sliderValue}%, #ccc ${sliderValue}%)`,
+          '--thumb-rotate': `${(sliderValue/100)*2160}deg`
+        }}
+        onInput={onInput}
+        value={sliderValue}
+      />
     </>
-    
-  )
+  );
 }
 
-export default RangeSlider
+export default RangeSlider;
